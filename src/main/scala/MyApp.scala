e8bf27d084dc6243544c53e9ab348d4d7628b0ad
Ieva Bumbure-Dz <bumbure.ieva@gmail.com>
2021-02-02 21:54:09 +0200

    Initial commit

---
+++ src/main/scala/MyApp.scala
@@ -0,0 +1,7 @@
+object MyApp extends App{
+  println("My first Scala program")
+  print("So print has no new line")
+  print("As can be seen here")
+  println() //just an empty line //is a comment for meaning for humans not for computer
+  println("Hmm more code!")
+}
